f3e138d9eb0e083473e9b43cbbd23ab4306e6ced
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-04-14 08:48:52 +0000

    e

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 class Mock:
 
   def __init__(self):
-    self.count = 0
+    self.__count = 0
 
   @property
   def count(self):
-    return self.count
+    return self.__count
 
   @count.setter
   def count(self, n):
-    self.count = n
+    self.__count = n
+
+
+mock = Mock()
+get = mock.count
+print(get)
+
+mock.count = 100
+print(get)
