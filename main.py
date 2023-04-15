893979710f8d24d31fe544530b5bb94386dc8643
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-04-15 09:52:46 +0000

    e

--- main.py
+++ main.py
@@ -13,3 +13,18 @@ def generate_combinations(arr, r):
                for subset in generate_combinations(tail, r - 1)]
 
   return with_head + without_head
+
+
+def main():
+  n = 6
+  r = 4
+
+  arr = list(range(1, n + 1))
+  combinations = generate_combinations(arr, r)
+  result = len(combinations)
+
+  print(f"{n}C{r}={result}")
+
+
+if __name__ == "__main__":
+  main()
