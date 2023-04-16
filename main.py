089f57dd021c5c61da06e687e8f0c8558a465f14
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-04-16 07:30:11 +0000

    e

--- main.py
+++ main.py
@@ -3,3 +3,48 @@ class ListNode:
   def __init__(self, data):
     self.data = data
     self.next = None
+
+
+class LinkedList:
+
+  def __init__(self):
+    self.head = None
+
+  def insert(self, data):
+    new_node = ListNode(data)
+    new_node.next = self.head
+    self.head = new_node
+
+  def delete(self, data):
+    current = self.head
+    prev = None
+
+    while current is not None:
+      if current.data == data:
+        break
+
+      prev = current
+      current = current.next
+
+    if current is None:
+      return "Value not found in the list."
+
+    if prev is None:
+      self.head = current.next
+    else:
+      prev.next = current.next
+
+  def search(self, data):
+    current = self.head
+    while current is not None:
+      if current.data == data:
+        return True
+      current = current.next
+    return False
+
+  def print_list(self):
+    current = self.head
+    while current is not None:
+      print(current.data, end=" -> ")
+      current = current.next
+    print("None")
