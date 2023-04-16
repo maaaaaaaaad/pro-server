be30700ca60e5837d5bd2af3cd96ffd4a7e2a0af
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-04-16 07:29:52 +0000

    ㄷ

--- main.py
+++ main.py
@@ -1,30 +1,5 @@
-def generate_combinations(arr, r):
-  if r == 0:
-    return [[]]
+class ListNode:
 
-  if not arr:
-    return []
-
-  head = arr[0]
-  tail = arr[1:]
-
-  without_head = generate_combinations(tail, r)
-  with_head = [[head] + subset
-               for subset in generate_combinations(tail, r - 1)]
-
-  return with_head + without_head
-
-
-def main():
-  n = 6
-  r = 4
-
-  arr = list(range(1, n + 1))
-  combinations = generate_combinations(arr, r)
-  result = len(combinations)
-
-  print(f"{n}C{r}={result}")
-
-
-if __name__ == "__main__":
-  main()
+  def __init__(self, data):
+    self.data = data
+    self.next = None
