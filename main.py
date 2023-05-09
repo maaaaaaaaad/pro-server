fcb362c6f351d6a3b385dcd8abc9248f2c1786d5
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-05-09 04:49:44 +0000

    e

--- main.py
+++ main.py
@@ -1,32 +1,14 @@
-class Animal:
+def solution(money):
+  n = len(money)
+  if n == 3:
+    return max(money)
+  dp1 = [0] * n
+  dp1[0], dp1[1] = money[0], max(money[0], money[1])
+  for i in range(2, n - 1):
+    dp1[i] = max(dp1[i - 2] + money[i], dp1[i - 1])
+  dp2 = [0] * n
+  dp2[0], dp2[1] = 0, money[1]
+  for i in range(2, n):
+    dp2[i] = max(dp2[i - 2] + money[i], dp2[i - 1])
 
-  def __init__(self, name):
-    self.name = name
-
-  def make_sound(self):
-    pass
-
-
-class Dog(Animal):
-
-  def __init__(self, name):
-    super().__init__(name)
-
-  def make_sound(self):
-    print("ziral!")
-
-
-class Cat(Animal):
-
-  def __init__(self, name):
-    super().__init__(name)
-
-  def make_sound(self):
-    print("ny~")
-
-
-d = Dog("b")
-c = Cat("n")
-
-d.make_sound()
-c.make_sound()
+  return max(dp1[n - 2], dp2[n - 1])
