e1c34ce991167a0565a328a952830ed4393df356
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-05-29 06:57:41 +0000

    e

--- main.py
+++ main.py
@@ -1,41 +1,39 @@
-import heapq
-
-INF = int(1e9)
-
-
-def dijkstra(start, graph, distance):
-  q = []
-  heapq.heappush(q, (0, start, 0))
-  distance[start][0] = 0
-  while q:
-    dist, now, trap_state = heapq.heappop(q)
-    if distance[now][trap_state] < dist:
-      continue
-    for i in graph[now][trap_state]:
-      cost = dist + i[1]
-      if cost < distance[i[0]][i[2]]:
-        distance[i[0]][i[2]] = cost
-        heapq.heappush(q, (cost, i[0], i[2]))
+from heapq import heappop, heappush
 
 
 def solution(n, start, end, roads, traps):
+  INF = int(1e9)
   start -= 1
   end -= 1
-  traps = [x - 1 for x in traps]
+  graph = [[[] for _ in range(n)] for _ in range(2)]
+  trap_set = set(traps)
 
-  graph = [[[], []] for _ in range(n)]
-  for road in roads:
-    x, y, cost = road
+  for x, y, cost in roads:
     x -= 1
     y -= 1
+    graph[0][x].append((y, cost))
+    graph[1][y].append((x, cost))
+    if x + 1 in trap_set:
+      graph[1][x].append((y, cost))
+    if y + 1 in trap_set:
+      graph[0][y].append((x, cost))
 
-    graph[x][0].append((y, cost, [0, 1][y in traps]))
-    graph[y][1].append((x, cost, [0, 1][x in traps]))
-    graph[y][0].append((x, cost, [0, 1][x in traps]))
-    graph[x][1].append((y, cost, [0, 1][y in traps]))
+  dist = [[INF] * n for _ in range(1 << len(trap_set))]
+  trap_dict = {trap - 1: i for i, trap in enumerate(traps)}
+  state = sum(1 << trap_dict[trap - 1] for trap in traps if trap - 1 < start)
+  dist[state][start] = 0
+  queue = [(0, start, state)]
 
-  distance = [[INF] * 2 for _ in range(n)]
-
-  dijkstra(start, graph, distance)
-
-  return min(distance[end])
+  while queue:
+    time, node, state = heappop(queue)
+    if node == end:
+      return time
+    trap_here = (1 << trap_dict[node]) if node + 1 in trap_set else 0
+    for next_node, cost in graph[state >> trap_dict[node]
+                                 & 1 if trap_here else state & 1 ^ 1][node]:
+      next_state = (state ^ trap_here) if (next_node +
+                                           1 in trap_set) else state
+      if time + cost < dist[next_state][next_node]:
+        dist[next_state][next_node] = time + cost
+        heappush(queue, (time + cost, next_node, next_state))
+  return -1
