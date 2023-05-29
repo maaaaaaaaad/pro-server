0926e1caa60ffeefdc9fb737ab479f86e8f4a55a
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-05-29 06:41:18 +0000

    e

--- main.py
+++ main.py
@@ -1,51 +1,39 @@
-import requests
-from bs4 import BeautifulSoup
+import heapq
 
-import requests
-from bs4 import BeautifulSoup
+INF = int(1e9)
 
 
-def get_kbo_ranking(start_year, end_year):
-  url = f"https://ko.wikipedia.org/wiki/KBO_%EB%A6%AC%EA%B7%B8_%EC%97%B0%EB%8F%84%EB%B3%84_%ED%8C%80_%EC%88%9C%EC%9C%84"
-  response = requests.get(url)
-  soup = BeautifulSoup(response.text, 'html.parser')
-  table = soup.find("table", {"class": "wikitable"})
-  rows = table.findAll("tr")[2:]  # 헤더 제외한 행 추출
+def solution(n, start, end, roads, traps):
+  graph = [[INF] * (n + 1) for _ in range(n + 1)]
+  trap_dict = {trap: [] for trap in traps}
+  rev_trap_dict = {trap: [] for trap in traps}
+  trap_state = [0] * (n + 1)
+  for road in roads:
+    P, Q, S = road
+    graph[P][Q] = min(graph[P][Q], S)
+    if Q in traps:
+      trap_dict[Q].append(P)
+    if P in traps:
+      rev_trap_dict[P].append(Q)
 
-  ranking = {}
-  for row in rows:
-    cols = row.findAll("td")
-    team_name = cols[1].get_text().strip()
-    rank = int(cols[0].get_text().strip())
-    year = int(cols[2].get_text().strip())
-    if year not in ranking:
-      ranking[year] = {}
-    ranking[year][team_name] = rank
-
-  return {year: ranking[year] for year in range(start_year, end_year + 1)}
-
-
-start_year = 2016
-end_year = 2020
-lotte_giants_rankings = []
-
-kbo_rankings = get_kbo_ranking(start_year, end_year)
-
-for year in range(start_year, end_year + 1):
-  lotte_giants_rank = kbo_rankings[year]["롯데 자이언츠"]
-  lotte_giants_rankings.append(lotte_giants_rank)
-
-print(lotte_giants_rankings)
-
-
-def calculate_probability(rankings, threshold):
-  total_years = len(rankings)
-  successful_years = sum(1 for rank in rankings if rank <= threshold)
-  probability = successful_years / total_years
-  return probability
-
-
-threshold = 5
-probability = calculate_probability(lotte_giants_rankings, threshold)
-
-print(f"롯데 자이언츠가 가을 야구에 진출할 확률: {probability * 100:.2f}%")
+  q = []
+  heapq.heappush(q, (0, start, tuple(trap_state)))
+  dist = {tuple(trap_state): [INF] * (n + 1)}
+  dist[tuple(trap_state)][start] = 0
+  while q:
+    time, now, trap_state = heapq.heappop(q)
+    trap_state = list(trap_state)
+    if time > dist[tuple(trap_state)][now]:
+      continue
+    if now in traps:
+      for node in trap_dict[now]:
+        graph[now][node], graph[node][now] = graph[node][now], graph[now][node]
+      for node in rev_trap_dict[now]:
+        graph[now][node], graph[node][now] = graph[node][now], graph[now][node]
+      trap_state[now] = 1 if trap_state[now] == 0 else 0
+    for i in range(1, n + 1):
+      cost = time + graph[now][i]
+      if cost < dist[tuple(trap_state)][i]:
+        dist[tuple(trap_state)][i] = cost
+        heapq.heappush(q, (cost, i, tuple(trap_state)))
+  return min(dist[tuple(trap_state)][end] for trap_state in dist.values())
