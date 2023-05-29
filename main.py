fd2e92d4b713d23e55538906a7675f616d74bf68
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-05-29 06:47:03 +0000

    e

--- main.py
+++ main.py
@@ -3,37 +3,39 @@ import heapq
 INF = int(1e9)
 
 
-def solution(n, start, end, roads, traps):
-  graph = [[INF] * (n + 1) for _ in range(n + 1)]
-  trap_dict = {trap: [] for trap in traps}
-  rev_trap_dict = {trap: [] for trap in traps}
-  trap_state = [0] * (n + 1)
-  for road in roads:
-    P, Q, S = road
-    graph[P][Q] = min(graph[P][Q], S)
-    if Q in traps:
-      trap_dict[Q].append(P)
-    if P in traps:
-      rev_trap_dict[P].append(Q)
-
+def dijkstra(start, graph, distance):
   q = []
-  heapq.heappush(q, (0, start, tuple(trap_state)))
-  dist = {tuple(trap_state): [INF] * (n + 1)}
-  dist[tuple(trap_state)][start] = 0
+  heapq.heappush(q, (0, start, 0))
+  distance[start][0] = 0
   while q:
-    time, now, trap_state = heapq.heappop(q)
-    trap_state = list(trap_state)
-    if time > dist[tuple(trap_state)][now]:
+    dist, now, trap_state = heapq.heappop(q)
+    if distance[now][trap_state] < dist:
       continue
-    if now in traps:
-      for node in trap_dict[now]:
-        graph[now][node], graph[node][now] = graph[node][now], graph[now][node]
-      for node in rev_trap_dict[now]:
-        graph[now][node], graph[node][now] = graph[node][now], graph[now][node]
-      trap_state[now] = 1 if trap_state[now] == 0 else 0
-    for i in range(1, n + 1):
-      cost = time + graph[now][i]
-      if cost < dist[tuple(trap_state)][i]:
-        dist[tuple(trap_state)][i] = cost
-        heapq.heappush(q, (cost, i, tuple(trap_state)))
-  return min(dist[tuple(trap_state)][end] for trap_state in dist.values())
+    for i in graph[now][trap_state]:
+      cost = dist + i[1]
+      if cost < distance[i[0]][i[2]]:
+        distance[i[0]][i[2]] = cost
+        heapq.heappush(q, (cost, i[0], i[2]))
+
+
+def solution(n, start, end, roads, traps):
+  start -= 1
+  end -= 1
+  traps = [x - 1 for x in traps]
+
+  graph = [[[], []] for _ in range(n)]
+  for road in roads:
+    x, y, cost = road
+    x -= 1
+    y -= 1
+
+    graph[x][0].append((y, cost, [0, 1][y in traps]))
+    graph[y][1].append((x, cost, [0, 1][x in traps]))
+    graph[y][0].append((x, cost, [0, 1][x in traps]))
+    graph[x][1].append((y, cost, [0, 1][y in traps]))
+
+  distance = [[INF] * 2 for _ in range(n)]
+
+  dijkstra(start, graph, distance)
+
+  return min(distance[end])
