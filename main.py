cd15137948ad13a2e7d87ee2a9e01784db824049
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-05-30 06:21:55 +0000

    ee

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-import sys
+import bisect
 
 
 def solution(land, P, Q):
-
-  def calculate_cost(height):
-    inc = sum(max(0, h - height) for h in flatten) * Q
-    dec = sum(max(0, height - h) for h in flatten) * P
-    return inc + dec
-
-  flatten = [h for row in land for h in row]
-  flatten.sort()
+  flatten = sorted(h for row in land for h in row)
+  N = len(flatten)
+  prefix_sum = [0] * (N + 1)
+  for i in range(N):
+    prefix_sum[i + 1] = prefix_sum[i] + flatten[i]
 
   left, right = flatten[0], flatten[-1]
-  answer = sys.maxsize
+  answer = float('inf')
+
+  def calculate_cost(mid):
+    idx = bisect.bisect_right(flatten, mid)
+    inc = prefix_sum[-1] - prefix_sum[idx] - (N - idx) * mid
+    dec = idx * mid - prefix_sum[idx]
+    return inc * Q + dec * P
 
   while left <= right:
     mid = (left + right) // 2
